f76c1a08a09caf3eea71df983c2461aeba306051
Swapnil Patle <swapnilpatle01@gmail.com>
2024-09-28 16:42:53 +0530

    test backend

--- backend/index.js
+++ backend/index.js
@@ -6,44 +6,44 @@ import connectDB from "./utils/db.js";
 //  import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
- import applicationRoute from "./routes/application.route.js";
+import applicationRoute from "./routes/application.route.js";
 
 //  const userRoute= require('./routes/userRoutes.js')
- import userRoute from './routes/userRoutes.js';
+import userRoute from "./routes/userRoutes.js";
 
-
-  dotenv.config({});
+dotenv.config({});
 
 const app = express();
 
-
 // middleware
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'http://localhost:5173',
-    credentials:true
-}
+  origin: "http://localhost:3000",
+  credentials: true,
+};
 
 app.use(cors(corsOptions));
 
-const PORT = process.env.PORT || 3000;
-
+const PORT = process.env.PORT || 5000;
 
 // api's
- app.use("/api/v1/user", userRoute);
+app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
- app.use("/api/v1/job", jobRoute);
+app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
 
+app.use((err, req, res, next) => {
+  console.error(err.stack); // Log the error details to the console
+  res.status(500).json({ message: "Server error" });
+});
 
-app.listen(PORT,()=>{
-    connectDB();
-    console.log(`Server running http://localhost:${PORT}`);
-})
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`Server running http://localhost:${PORT}`);
+});
 
-app.get('/', (req,res)=>{
-    res.send('Engineer Study Hub')
-    
-})
+app.get("/", (req, res) => {
+  res.send("Engineer Study Hub");
+});
